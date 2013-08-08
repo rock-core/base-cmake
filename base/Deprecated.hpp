cdea1aabf5e2525672ab3bb6a9b9beb706a557d7
Thomas Roehr <thomas.roehr@dfki.de>
2013-08-08 11:20:52 +0200

    Transition to CMake only package

--- base/Deprecated.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef BASE_TYPES_DEPRECATED
-#ifdef __GNUC__
-    #define BASE_TYPES_DEPRECATED __attribute__ ((deprecated))
-#else
-    #define BASE_TYPES_DEPRECATED
-#endif 
-#endif
-
--- src/Logging.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef _BASE_LOGGING_H_
-#define _BASE_LOGGING_H_
-#include <base/logging/logging_printf_style.h>
-#include <base/logging/logging_iostream_style.h>
-#endif // _BASE_LOGGING_H_
