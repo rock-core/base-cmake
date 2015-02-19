a31ed75aac545024381c873b29b02d053439d467
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-02-19 18:06:45 +0100

    Added macro for CXX11 activation (not enabled by default (so far))

--- modules/Rock.cmake
+++ modules/Rock.cmake
@@ -1,3 +1,17 @@
+macro(rock_activate_cxx11)
+    include(CheckCXXCompilerFlag)
+    CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+    CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+    if(COMPILER_SUPPORTS_CXX11)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    elseif(COMPILER_SUPPORTS_CXX0X)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    else()
+        message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+    endif()
+endmacro()
+
+
 macro(rock_use_full_rpath install_rpath)
     # use, i.e. don't skip the full RPATH for the build tree
     SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
