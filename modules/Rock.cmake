b0af19c84ce57142dbd4a0fefac0bf93f5ad68a7
Gabriel Arjones <arjones@arjones.com>
2016-09-13 17:07:49 -0300

    Prefix macro with rock_

--- modules/Rock.cmake
+++ modules/Rock.cmake
@@ -49,18 +49,18 @@ function(rock_add_compiler_flag_if_it_exists FLAG)
     endif()
 endfunction()
 
-macro(ADD_TARGET_PROPERTIES _target _name _properties)
+macro(rock_add_target_properties _target _name _properties)
     set(_properties ${ARGV})
     list(REMOVE_AT _properties 0)
     list(REMOVE_AT _properties 0)
     get_target_property(_old_properties ${_target} ${_name})
     if(NOT _old_properties)
         set(_old_properties)
-    else(NOT _old_properties)
+    else()
         set(_old_properties "${_old_properties} ")
-    endif(NOT _old_properties)
+    endif()
     set_target_properties(${_target} PROPERTIES ${_name} "${_old_properties}${_properties}")
-endmacro(ADD_TARGET_PROPERTIES)
+endmacro()
 
 function(rock_add_compiler_flag_to_target_if_it_exists ROCK_TARGET FLAG)
     string(REGEX REPLACE "[^a-zA-Z]"
@@ -68,8 +68,8 @@ function(rock_add_compiler_flag_to_target_if_it_exists ROCK_TARGET FLAG)
         "${FLAG}")
     CHECK_CXX_COMPILER_FLAG(${FLAG} CXX_SUPPORTS${VAR_SUFFIX})
     if (CXX_SUPPORTS${VAR_SUFFIX})
-        add_target_properties(${ROCK_TARGET}
-                              COMPILE_FLAGS ${FLAG})
+        rock_add_target_properties(${ROCK_TARGET}
+                                   COMPILE_FLAGS ${FLAG})
     endif()
 endfunction()
 
